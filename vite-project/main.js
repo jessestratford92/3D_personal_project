dc59cee1ceec56eb34d345198191b07881a9310d
Jesse Stratford <98693587+jessestratford92@users.noreply.github.com>
2022-05-17 18:32:24 +1200

    3d box animation showing - animated

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { BoxGeometry } from 'three';
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
@@ -14,4 +15,31 @@ console.log(scene)
 console.log(camera)
 console.log(renderer)
 
+renderer.setSize(innerWidth, innerHeight)
 document.body.appendChild(renderer.domElement)
+
+const boxGeometry = new THREE.BoxGeometry
+    (1,1,1)
+
+console.log(boxGeometry)
+
+const material = new THREE.MeshBasicMaterial({color: 0x00F00})
+ 
+console.log(material)
+
+const mesh = new THREE.Mesh(boxGeometry, material)
+
+console.log(mesh)
+
+scene.add(mesh)
+
+camera.position.z = 5
+
+function animate () {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+  mesh.rotation.x += 0.01
+  mesh.rotation.y += 0.01
+}
+
+animate ()
