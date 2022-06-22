ec45759d140d25fc5b6eca6f89629d7c2ba904d2
Jesse Stratford <98693587+jessestratford92@users.noreply.github.com>
2022-06-22 17:47:18 +1200

    audio listener added - working with buffer time though

--- vite-project/main.js
+++ vite-project/main.js
@@ -28,6 +28,22 @@ function generatePlane() {
     world.plane.heightSegments
   )
 
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+const sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load('sounds/opening.mp3', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop(true);
+	sound.setVolume(0.5);
+	sound.play();
+});
+
   // vertice position randomization
   const { array } = planeMesh.geometry.attributes.position
   const randomValues = []
