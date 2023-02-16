11785844771db58ed39e276b08a206343ac64d81
Jesse Stratford <98693587+jessestratford92@users.noreply.github.com>
2023-02-16 16:58:00 +1300

    add in curtain effect loading animation

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,8 +1,47 @@
-import * as THREE from 'https://unpkg.com/three@0.142.0/build/three.js';
+import * as THREE from 'three'
 import * as dat from 'https://cdn.jsdelivr.net/npm/dat.gui@0.7.9/build/dat.gui.module.js';
 import gsap from 'gsap/all';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls';
 
+/** @format */
+const svg = document.getElementById("svg");
+const tl = gsap.timeline();
+const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+
+tl.from(".loader-wrap-heading h1", {
+  delay: 1,
+  y: 200,
+  skewY: 10,
+}).to(".loader-wrap-heading h1", {
+  delay: 1.5,
+  y: -200,
+  skewY: 10,
+});
+tl.to(svg, {
+  duration: 0.8,
+  attr: { d: curve },
+  ease: "power2.easeIn",
+}).to(svg, {
+  duration: 0.8,
+  attr: { d: flat },
+  ease: "power2.easeOut",
+});
+tl.to(".loader-wrap", {
+  y: -1500,
+});
+tl.to(".loader-wrap", {
+  zIndex: -1,
+  display: "none",
+});
+tl.from(
+  ".container h1",
+  {
+    y: 100,
+    opacity: 0,
+  },
+  "-=1.5"
+);
 
 const gui = new dat.GUI()
 const world = {
